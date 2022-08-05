022a07d4efd98b9bf9d80354cc041411bba8ae25
Israeltheminer <israeliyanda5@gmail.com>
2022-08-05 19:27:21 +0100

    rectify bug on filtering system

--- src/redux/ProfileSlice.ts
+++ src/redux/ProfileSlice.ts
@@ -18,12 +18,27 @@ export interface ProfileState {
 	}[];
 }
 
+type ResultType = {
+	id: number;
+	company: string;
+	new: boolean;
+	featured: boolean;
+	position: string;
+	role: string;
+	level: string;
+	postedAt: string;
+	contract: string;
+	location: string;
+	languages: string[];
+	tools: string[];
+}[];
+
 const initialState: ProfileState = {
 	profiles: data,
 };
 
-type Tag = { key: string; bracket: string };
 type AllTags = { key: string; bracket: string }[];
+type tags = { key: string; bracket: string };
 
 export const ProfileSlice = createSlice({
 	name: "profiles",
@@ -34,20 +49,23 @@ export const ProfileSlice = createSlice({
 		},
 		filter: (state, action: PayloadAction<AllTags>) => {
 			let allTags = action.payload;
-			let previousProfiles = data;
-			allTags.forEach((tag) => {
+			let previousProfiles = state.profiles;
+			let initialProfiles = data;
+			let filteredResult: ResultType | any;
+			const filterTag = (tag: tags, profiles: ResultType): ResultType => {
+				let result: ResultType | any;
 				if (tag.bracket === "Level") {
-					let newProfilesByLevel = previousProfiles.filter(
+					let newProfilesByLevel = profiles.filter(
 						(profile): boolean => profile.level === tag.key
 					);
-					state.profiles = newProfilesByLevel;
+					result = newProfilesByLevel;
 				} else if (tag.bracket === "Role") {
-					let newProfilesByRole = previousProfiles.filter(
+					let newProfilesByRole = profiles.filter(
 						(profile): boolean => profile.role === tag.key
 					);
-					state.profiles = newProfilesByRole;
+					result = newProfilesByRole;
 				} else if (tag.bracket === "Tools") {
-					let newProfilesByTools = previousProfiles.filter((profile): boolean => {
+					let newProfilesByTools = profiles.filter((profile): boolean => {
 						let tools = profile.tools;
 						let tool = "";
 						for (let i = 0; i < tools.length; i++) {
@@ -57,9 +75,9 @@ export const ProfileSlice = createSlice({
 						}
 						return tool === tag.key;
 					});
-					state.profiles = newProfilesByTools;
+					result = newProfilesByTools;
 				} else if (tag.bracket === "Languages") {
-					let newProfilesByLanguages = previousProfiles.filter((profile): boolean => {
+					let newProfilesByLanguages = profiles.filter((profile): boolean => {
 						let languages = profile.languages;
 						let language = "";
 						for (let i = 0; i < languages.length; i++) {
@@ -69,53 +87,23 @@ export const ProfileSlice = createSlice({
 						}
 						return language === tag.key;
 					});
-					state.profiles = newProfilesByLanguages;
+					result = newProfilesByLanguages;
 				}
-			});
-		},
-		singleTagFilter: (state, action: PayloadAction<Tag>) => {
-			let tagKey = action.payload.key;
-			let tagBracket = action.payload.bracket;
-			let previousProfiles = state.profiles;
-			if (tagBracket === "Level") {
-				let newProfilesByLevel = previousProfiles.filter(
-					(profile): boolean => profile.level === tagKey
-				);
-				state.profiles = newProfilesByLevel;
-			} else if (tagBracket === "Role") {
-				let newProfilesByRole = previousProfiles.filter(
-					(profile): boolean => profile.role === tagKey
-				);
-				state.profiles = newProfilesByRole;
-			} else if (tagBracket === "Tools") {
-				let newProfilesByTools = previousProfiles.filter((profile): boolean => {
-					let tools = profile.tools;
-					let tool = "";
-					for (let i = 0; i < tools.length; i++) {
-						if (profile.tools[i] === tagKey) {
-							tool = profile.tools[i];
-						}
-					}
-					return tool === tagKey;
-				});
-				state.profiles = newProfilesByTools;
-			} else if (tagBracket === "Languages") {
-				let newProfilesByLanguages = previousProfiles.filter((profile): boolean => {
-					let languages = profile.languages;
-					let language = "";
-					for (let i = 0; i < languages.length; i++) {
-						if (profile.languages[i] === tagKey) {
-							language = profile.languages[i];
-						}
-					}
-					return language === tagKey;
-				});
-				state.profiles = newProfilesByLanguages;
+				return result;
+			};
+			for (let i = 0; i < allTags.length; i++) {
+				let tag = allTags[i];
+				if (i === 0) {
+					filteredResult = filterTag(tag, initialProfiles);
+				} else {
+					filteredResult = filterTag(tag, previousProfiles);
+				}
 			}
+			state.profiles = filteredResult;
 		},
 	},
 });
 
-export const { singleTagFilter, resetProfiles, filter } = ProfileSlice.actions;
+export const { resetProfiles, filter } = ProfileSlice.actions;
 
 export default ProfileSlice.reducer;
